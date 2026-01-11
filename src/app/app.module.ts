23d9380e921bce0eb7bbac26900b907ed1d56b39
Adones Balica <adonesbalica@gmail.com>
2026-01-11 10:27:44 -0300

    feat: connecting to database and validate .env file

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule, type ConfigType } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import appConfig from "./app.config";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+
+@Module({
+	imports: [
+		ConfigModule.forRoot(),
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule.forFeature(appConfig)],
+			inject: [appConfig.KEY],
+			useFactory: async (appConfigurations: ConfigType<typeof appConfig>) => {
+				return {
+					type: appConfigurations.database.type,
+					host: appConfigurations.database.host,
+					port: appConfigurations.database.port,
+					username: appConfigurations.database.username,
+					database: appConfigurations.database.database,
+					password: appConfigurations.database.password,
+					autoLoadEntities: appConfigurations.database.autoLoadEntities,
+					synchronize: appConfigurations.database.synchronize,
+				};
+			},
+		}),
+	],
+	controllers: [AppController],
+	providers: [AppService],
+})
+export class AppModule {}
